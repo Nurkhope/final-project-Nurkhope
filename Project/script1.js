f0dfd70bb78703560cd5e5fdfcc7d72e972af692
Nurkhat <tugelbayev.n@gmail.com>
2020-11-28 23:10:16 +0600

    Created page_advert and added cards to page_cars

--- Project/script1.js
+++ Project/script1.js
@@ -32,6 +32,7 @@ function setCursorPosition(pos, elem) {
     }
 }
 
+/*Mask for phone number */
 function mask(event) {
     var matrix = "+7 (___) ___ ____",
         i = 0,
@@ -50,3 +51,26 @@ function mask(event) {
     input.addEventListener("focus", mask, false);
     input.addEventListener("blur", mask, false);
 });
+
+/*PAGE ADVERT script for images*/
+document.querySelector("#i1").addEventListener('click', click1);
+document.querySelector("#i2").addEventListener('click', click2);
+document.querySelector("#i3").addEventListener('click', click3);
+document.querySelector("#i4").addEventListener('click', click4);
+document.querySelector("#i5").addEventListener('click', click5);
+
+function click1(){
+	document.querySelector("#bigImage img").src = document.querySelector("#i1").src;
+}
+function click2(){
+	document.querySelector("#bigImage img").src = document.querySelector("#i2").src;
+}
+function click3(){
+	document.querySelector("#bigImage img").src = document.querySelector("#i3").src;
+}
+function click4(){
+	document.querySelector("#bigImage img").src = document.querySelector("#i4").src;
+}
+function click5(){
+	document.querySelector("#bigImage img").src = document.querySelector("#i5").src;
+};
